746d73a4ff09bab869d74ef10e748d32bb4072a1
fatehyab31 <fatehyabajaz31@gmail.com>
2026-05-19 17:26:02 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+// PF POST MID PROJECT.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<fstream> //without this we cant read or acess the struture.
+using namespace std;
+//struct data {
+//    string title;
+//    string isbn;
+//    string author;
+//    string publisher;
+//    int copies;
+//    int edition;
+//};
+
+//int addBook()
+//int deleteBook()
+//int modifyBook()
+//int searchBookByISBN()
+//int searchBookByTitle()
+//int sortByTitle()
+//int sortByEdition()
+//int borrowBook()
+//int returnBook()
+//int displayBooks()
+
+
+int main() {
+	cout << "Hey there!Tell me what do you want to do?" << endl;
+
+ return 0;
+
+
+}
